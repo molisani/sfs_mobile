25c9b5ce3a41bd2c63a3e5b51eb66d1332b41e92
Michael Molisani <molisani@seas.upenn.edu>
2014-04-07 19:31:08 -0400

    Re-organized test.

--- sfs_mobile/src/edu/upenn/cis350/sfs_mobile/tests/ServerPOSTTest.java
+++ sfs_mobile/src/edu/upenn/cis350/sfs_mobile/tests/ServerPOSTTest.java
@@ -5,11 +5,18 @@ import static org.junit.Assert.*;
 import org.junit.Before;
 import org.junit.Test;
 
+import edu.upenn.cis350.sfs_mobile.ServerPOST;
+
 public class ServerPOSTTest {
 
 	@Test
-	public void testExecute() {
-		assertTrue(true);
+	public void testLogin() {
+		ServerPOST post = new ServerPOST("auth.php");
+		post.addField("pennkey", "molisani");
+		post.addField("password", "mpassword");
+		post.addField("birthday", "1994-01-31");
+		post.execute();
+		System.out.println(post.getResult().toString());
 	}
 	
 	
