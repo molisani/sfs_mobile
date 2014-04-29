a5f1fce55cc8f10cd64fc6447110f37784c67915
Michael Molisani <molisani@seas.upenn.edu>
2014-04-29 12:45:40 -0400

    fixed server op field

--- sfs_mobile/src/edu/upenn/cis350/sfs_mobile/ServerLogin.java
+++ sfs_mobile/src/edu/upenn/cis350/sfs_mobile/ServerLogin.java
@@ -1,6 +1,7 @@
 public class ServerLogin extends ServerPOST {
     public ServerLogin(String user, String pass, int year, int month, int day) {
         super("auth.php");
+        addField("login", "");
         addField("pennkey", user);
         addField("password", pass);
         addField("birthday", String.format("%d-%d-%d", year, month, day));
