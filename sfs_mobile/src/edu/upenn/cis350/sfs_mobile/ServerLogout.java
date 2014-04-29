bfe32961a9813b33687220b538bcc9d339f2f5bb
Michael Molisani <molisani@seas.upenn.edu>
2014-04-29 12:48:51 -0400

    only 5 lines

--- sfs_mobile/src/edu/upenn/cis350/sfs_mobile/ServerLogout.java
+++ sfs_mobile/src/edu/upenn/cis350/sfs_mobile/ServerLogout.java
@@ -1,8 +1,5 @@
 public class ServerLogout extends ServerPOST {
     public ServerLogout(String user, String token) {
-        super("auth.php");
-        addField("logout", "");
-        addFIeld("pennkey", user);
-        addField("auth_token", token);
+        super("auth.php", "logout", "", "pennkey", user, "auth_token", token);
     }
 }
