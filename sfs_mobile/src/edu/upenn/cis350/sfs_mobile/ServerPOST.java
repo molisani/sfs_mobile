34dbc16b20d31d0fb61c61f99c2ae1f040f8f6b0
ejiang9 <ejiang9@gmail.com>
2014-04-29 14:19:52 -0400

    Revert "Michael's post change was giving me some errors"
    This reverts commit c2bf8cbd53f46ad6f1ab93b6f6618632e3fd7bd2.

--- sfs_mobile/src/edu/upenn/cis350/sfs_mobile/ServerPOST.java
+++ sfs_mobile/src/edu/upenn/cis350/sfs_mobile/ServerPOST.java
@@ -32,8 +32,7 @@ public class ServerPOST {
 	}
 	
 	public ServerPOST(String php, String... keyvals){
-		url = "https://fling.seas.upenn.edu/~molisani/cgi-bin/" + php;
-		query = new ArrayList<NameValuePair>();
+		ServerPOST(php);
 		if (keyvals.length % 2 != 1) {
 			for (int i = 0; i < keyvals.length; i += 2) {
 				addField(keyvals[i], keyvals[i + 1]);
