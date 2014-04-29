bbff4313b7598896ec5d165e2bda47e629f302c4
Michael Molisani <molisani@seas.upenn.edu>
2014-04-29 12:42:48 -0400

    better standalone server login class

---
+++ sfs_mobile/src/edu/upenn/cis350/sfs_mobile/ServerLogin.java
@@ -0,0 +1,8 @@
+public class ServerLogin extends ServerPOST {
+    public ServerLogin(String user, String pass, int year, int month, int day) {
+        super("auth.php");
+        addField("pennkey", user);
+        addField("password", pass);
+        addField("birthday", String.format("%d-%d-%d", year, month, day));
+    }
+}
